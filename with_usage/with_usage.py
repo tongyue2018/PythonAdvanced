23a94c20de0362a38551694da062f26642a91b77
tongyue2018 <tongyue2018@163.com>
2020-03-30 00:17:26 +0800

    nohup

--- with_usage/with_usage.py
+++ with_usage/with_usage.py
@@ -3,13 +3,16 @@
 
 # 函数如果实现了 __enter__, __exit__两个函数，就可以用with语句，with会自动在执行内容前后调用这2个方法
 
-#常用于 执行sql，需要释放资源
-
+# 1 常用于 执行sql，需要释放资源
+# 2 另常用于文件读写
+# 3 处理异常
 class shangxue():
     def __enter__(self):
         print('代码执行前------------')
+        return "我是as后面的变量 f--------"
     def __exit__(self, exc_type, exc_val, exc_tb):
         print('代码执行后------------')
 
-with shangxue():
+with shangxue() as f:  # f是__enter__的返回值
     print('代码执行中')
+    print(f)
