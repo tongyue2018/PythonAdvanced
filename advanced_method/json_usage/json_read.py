0363c803bf148ea0ab69f7ffe201fc52a09e1a02
tongyue <tongyue2018@163.com>
2020-04-14 09:00:58 +0800

    nohup111

--- advanced_method/json_usage/json_read.py
+++ advanced_method/json_usage/json_read.py
@@ -8,3 +8,11 @@ with open('../data/json.txt','r',encoding="utf-8") as f:
     data = f.read()
     dataChuli = json.loads(data)
     print(dataChuli)
+
+
+#方法二 将文件流传给json.load(文件流)s
+
+with open('../data/json.txt','r',encoding="utf-8") as f:
+    dataJsonList=json.load(f)
+    print(type(dataJsonList))
+    print(dataJsonList)
