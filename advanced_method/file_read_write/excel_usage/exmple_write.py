ac060477239334af95974afd568bd2dea62a4851
tongyue <tongyue2018@163.com>
2020-05-11 17:05:51 +0800

    nohup111

--- advanced_method/file_read_write/excel_usage/exmple_write.py
+++ advanced_method/file_read_write/excel_usage/exmple_write.py
@@ -12,8 +12,11 @@ dest_filename = 'empty_book.xlsx'
 ws1 = wb.active
 ws1.title = "range names"
 
+listData = ['a','b','c']
+
 for row in range(1, 40):
-    ws1.append(range(600))
+    # ws1.append(range(600))
+    ws1.append(listData)
 
 ws2 = wb.create_sheet(title="Pi")
 
@@ -24,5 +27,4 @@ for row in range(10, 20):
      for col in range(27, 54):
          _ = ws3.cell(column=col, row=row, value="{0}".format(get_column_letter(col)))
 print(ws3['AA10'].value)
-
 wb.save(filename = dest_filename)
