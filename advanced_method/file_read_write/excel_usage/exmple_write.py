056e231a4d0e62aca8c159b44cca5c2d21af921a
tongyue <tongyue2018@163.com>
2020-05-11 17:17:33 +0800

    nohup111

--- advanced_method/file_read_write/excel_usage/exmple_write.py
+++ advanced_method/file_read_write/excel_usage/exmple_write.py
@@ -25,6 +25,6 @@ ws2['F5'] = 3.14
 ws3 = wb.create_sheet(title="Data")
 for row in range(10, 20):
      for col in range(27, 54):
-         _ = ws3.cell(column=col, row=row, value="{0}".format(get_column_letter(col)))
+         ws3.cell(column=col, row=row, value=get_column_letter(col))
 print(ws3['AA10'].value)
 wb.save(filename = dest_filename)
