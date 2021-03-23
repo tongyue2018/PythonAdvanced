86bd858ceff53c0cf16de579bb24c65121781fb4
tongyue2018 <tongyue2018@163.com>
2021-03-23 20:38:43 +0800

    20210322

---
+++ advanced_method/requests/requests-get.py
@@ -0,0 +1,22 @@
+import requests,json,chardet
+
+
+urlAddress = 'http://47.107.58.164:5000/regist'
+
+params = {
+    'username':'大牛',
+    'passwd':'Qwe123%25%25%25'
+}
+
+headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "headerName":"headerName"
+}
+
+#表单提交  注意:1."Content-Type": "application/x-www-form-urlencoded"
+responseDataBiaodan = requests.get(url=urlAddress,params=params,headers=headers)
+dataStr = responseDataBiaodan.json()
+
+#直接打印中文乱码，json序列化可以解决, 简介的方法是responseDataJson.json()
+# dic = json.loads(dataStr)
+print(dataStr)
