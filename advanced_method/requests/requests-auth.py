abe7d2a92f420e0afc1d79b223eca47bf733baa8
tongyue2018 <tongyue2018@163.com>
2021-03-24 09:52:18 +0800

    20210322

--- advanced_method/requests/requests-auth.py
+++ advanced_method/requests/requests-auth.py
@@ -2,6 +2,6 @@ import requests,json,chardet
 from requests.auth import HTTPBasicAuth
 urlAddress = 'https://httpbin.testing-studio.com/basic-auth/banana/123'
 
-#方式1
+#有的接口需要认证才可以访问
 responseData1 = requests.get(url=urlAddress,auth=HTTPBasicAuth("banana","123"))
 print(responseData1.text)
